00bbd1a099394e3ad89ca1c8f2a4244b367289d0
Serge <iefserge@runtimejs.org>
2014-06-18 01:15:54 +0300

    apache 2.0 license

--- deps/libcxx/include/libcxx-runtimejs.h
+++ deps/libcxx/include/libcxx-runtimejs.h
@@ -1,7 +1,3 @@
-// Copyright 2014, runtime.js project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #pragma once
 
 // Use libc++ in Emscripten mode
