3eae788c4e7a54df6f82a79089e6498d38849126
Serge <iefserge@runtimejs.org>
2015-05-17 16:52:08 +0300

    es6 UDPSocket

--- js/core/net/udp-socket.js
+++ js/core/net/udp-socket.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+'use strict';
 var assertError = require('assert-error');
 var IP4Address = require('./ip4-address');
 var portUtils = require('./port-utils');
@@ -24,67 +25,70 @@ var route = require('./route');
 
 var ports = new PortPool();
 
-function UDPSocket() {
-  this._intf = null;
-  this._port = 0;
-  this.onReceive = new EventController();
-}
-
-UDPSocket.prototype.send = function(ip, port, u8) {
-  assertError(ip instanceof IP4Address, netError.E_IPADDRESS_EXPECTED);
-  assertError(portUtils.isPort(port), netError.E_INVALID_PORT);
-  assertError(u8 instanceof Uint8Array, netError.E_TYPEDARRAY_EXPECTED);
-
-  var intf = this._intf || null;
-
-  var viaIP;
-  if (ip.isBroadcast()) {
-    viaIP = ip;
-  } else {
-    var routingEntry = route.lookup(ip);
-    if (!routingEntry) {
-      throw netError.E_NO_ROUTE_TO_HOST;
-    }
-
-    viaIP = routingEntry.gateway;
-    if (!intf) {
-      intf = routingEntry.intf;
-    }
+class UDPSocket {
+  constructor(protocol) {
+    this._protocol = protocol || 'ip4';
+    this._intf = null;
+    this._port = 0;
+    this.onReceive = new EventController();
   }
 
-  if (!this._port) {
-    this._port = ports.getEphemeral(this);
+  send(ip, port, u8) {
+    assertError(ip instanceof IP4Address, netError.E_IPADDRESS_EXPECTED);
+    assertError(portUtils.isPort(port), netError.E_INVALID_PORT);
+    assertError(u8 instanceof Uint8Array, netError.E_TYPEDARRAY_EXPECTED);
+
+    var intf = this._intf || null;
+
+    var viaIP;
+    if (ip.isBroadcast()) {
+      viaIP = ip;
+    } else {
+      var routingEntry = route.lookup(ip);
+      if (!routingEntry) {
+        throw netError.E_NO_ROUTE_TO_HOST;
+      }
+
+      viaIP = routingEntry.gateway;
+      if (!intf) {
+        intf = routingEntry.intf;
+      }
+    }
+
     if (!this._port) {
-      throw netError.E_NO_FREE_PORT;
+      this._port = ports.getEphemeral(this);
+      if (!this._port) {
+        throw netError.E_NO_FREE_PORT;
+      }
+    }
+
+    udpTransmit(intf, ip, viaIP, this._port, port, u8);
+  }
+
+  bind(port) {
+    assertError(portUtils.isPort(port), netError.E_INVALID_PORT);
+
+    if (!ports.alloc(port, this)) {
+      throw netError.E_ADDRESS_IN_USE;
+    }
+
+    this._port = port;
+  }
+
+  bindToInterface(intf, port) {
+    if (!intf) {
+      throw netError.E_INTERFACE_EXPECTED;
+    }
+
+    this._intf = intf;
+    if (port) {
+      this.bind(port);
     }
   }
 
-  udpTransmit(intf, ip, viaIP, this._port, port, u8);
-};
-
-UDPSocket.prototype.bind = function(port) {
-  assertError(portUtils.isPort(port), netError.E_INVALID_PORT);
-
-  if (!ports.alloc(port, this)) {
-    throw netError.E_ADDRESS_IN_USE;
+  static lookupReceive(destPort) {
+    return ports.get(destPort) || null;
   }
-
-  this._port = port;
-};
-
-UDPSocket.prototype.bindToInterface = function(intf, port) {
-  if (!intf) {
-    throw netError.E_INTERFACE_EXPECTED;
-  }
-
-  this._intf = intf;
-  if (port) {
-    this.bind(port);
-  }
-};
-
-UDPSocket.lookupReceive = function(destPort) {
-  return ports.get(destPort) || null;
-};
+}
 
 module.exports = UDPSocket;
