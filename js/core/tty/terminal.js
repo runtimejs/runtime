4febd34ee60ff49b8c7ed9675aa81d6f5cc7d443
Ariel Abreu <facekapow2002@gmail.com>
2015-06-19 10:46:01 -0400

    Add a get function

--- js/core/tty/terminal.js
+++ js/core/tty/terminal.js
@@ -14,6 +14,7 @@
 
 'use strict';
 var vga = require('./vga');
+var EventController = require('event-controller');
 var buffer = vga.allocBuffer();
 buffer.clear(vga.color.BLACK);
 
@@ -37,7 +38,7 @@ exports.color = vga.color;
 exports.print = function(text, repeat, fg, bg) {
   // fix issue #53 where non-strings (ints, etc...) would not get printed
   text = String(text);
-  
+
   repeat = repeat || 1;
 
   if (typeof fg === 'undefined') {
@@ -67,6 +68,147 @@ exports.print = function(text, repeat, fg, bg) {
 
   refresh();
 };
+
+exports.get = function() {
+  var tty = runtime.tty;
+  var inputText = '';
+  var inputPosition = 0;
+  var history = [''];
+  var historyPosition = 0;
+  var done = false;
+  var inputEnabled = true;
+  var endresult = '';
+  var controller = new EventController();
+
+  function drawCursor() {
+    var char = ' ';
+    if (inputPosition < inputText.length) {
+      char = inputText[inputPosition];
+    }
+
+    tty.print(char, 1, tty.color.WHITE, tty.color.LIGHTGREEN);
+    tty.moveOffset(-1);
+  }
+
+  function removeCursor() {
+    var char = ' ';
+    if (inputPosition < inputText.length) {
+      char = inputText[inputPosition];
+    }
+
+    tty.print(char, 1, tty.color.WHITE, tty.color.BLACK);
+    tty.moveOffset(-1);
+  }
+
+  function putChar(char) {
+    removeCursor();
+    if (inputPosition >= inputText.length) {
+      inputText += char;
+      tty.print(char);
+    } else {
+      var rightSide = inputText.slice(inputPosition);
+      inputText = inputText.slice(0, inputPosition) + char + rightSide;
+      tty.print(char);
+      for (var i = 0; i < rightSide.length; ++i) {
+        tty.print(rightSide[i]);
+      }
+      tty.moveOffset(-rightSide.length);
+    }
+    ++inputPosition;
+    drawCursor();
+  }
+
+  function removeChar() {
+    if (inputPosition > 0) {
+      removeCursor();
+      if (inputPosition >= inputText.length) {
+        inputText = inputText.slice(0, -1);
+        tty.moveOffset(-1);
+      } else {
+        var rightSide = inputText.slice(inputPosition);
+        inputText = inputText.slice(0, inputPosition - 1) + rightSide;
+        tty.moveOffset(-1);
+        for (var i = 0; i < rightSide.length; ++i) {
+          tty.print(rightSide[i]);
+        }
+        tty.print(' ');
+        tty.moveOffset(-rightSide.length - 1);
+      }
+      --inputPosition;
+      drawCursor();
+    }
+  }
+
+  function newLine() {
+    var text = inputText;
+    inputEnabled = false;
+    historyPosition = 0;
+    removeCursor();
+    tty.print('\n');
+
+    inputText = '';
+    inputPosition = 0;
+
+    var result = text.trim();
+    if (result.length > 0) {
+      var currentIndex = history.indexOf(result);
+      if (currentIndex >= 0) {
+        history.splice(currentIndex, 1);
+      }
+      history.splice(1, 0, result);
+
+      runtime.keyboard.onKeydown.remove(addinput);
+      controller.dispatch(result);
+    } else {
+      runtime.keyboard.onKeydown.remove(addinput);
+      controller.dispatch(result);
+    }
+  }
+
+  function addinput(keyinfo) {
+    if (!inputEnabled) {
+      return;
+    }
+
+    switch (keyinfo.type) {
+    case 'kpup':
+      if (historyPosition < history.length - 1) {
+        setInputBox(history[++historyPosition]);
+      }
+      break;
+    case 'kpdown':
+      if (historyPosition > 0) {
+        setInputBox(history[--historyPosition]);
+      }
+      break;
+    case 'kpleft':
+      historyPosition = 0;
+      moveCursorLeft();
+      break;
+    case 'kpright':
+      historyPosition = 0;
+      moveCursorRight();
+      break;
+    case 'character':
+      historyPosition = 0;
+      putChar(keyinfo.character);
+      break;
+    case 'backspace':
+      historyPosition = 0;
+      removeChar();
+      break;
+    case 'enter':
+      newLine();
+      break;
+    }
+  }
+
+  runtime.keyboard.onKeydown.add(addinput);
+
+  drawCursor();
+
+  return controller;
+}
 
 exports.moveOffset = function(offset) {
   offset = offset | 0;
