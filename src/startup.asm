98c90ea47830cbcf6157ed85c26a5f28a47ddd28
Serge <iefserge@runtimejs.org>
2015-11-01 16:55:04 +0200

    Rename project in copyright headers in cc files

--- src/startup.asm
+++ src/startup.asm
@@ -1,4 +1,4 @@
-; Copyright 2014 Runtime.JS project authors
+; Copyright 2014 runtime.js project authors
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
