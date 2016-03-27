268f9e3f32a1a06f86fe30aa2378cef45438a1d8
Serge <iefserge@runtimejs.org>
2016-03-27 21:50:58 +0300

    Fix release id

--- scripts/helpers/release-id.js
+++ scripts/helpers/release-id.js
@@ -3,13 +3,13 @@ var assert = require('assert');
 function versionToReleaseId(ver) {
   var parts = String(ver).split('.');
   assert(parts.length === 3);
-  var major = parts[0];
-  var minor = parts[1];
-  var patch = parts[2];
+  var major = Number(parts[0]);
+  var minor = Number(parts[1]);
+  var patch = Number(parts[2]);
   assert(major < 1024);
   assert(minor < 1024);
   assert(patch < 1024);
-  return ((major << 20) + (minor << 10) + patch) >>> 0;
+  return ((major << 20) | (minor << 10) | patch) >>> 0;
 }
 
 module.exports = versionToReleaseId;
