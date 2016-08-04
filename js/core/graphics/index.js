7660783123e597838cd698e3ea3f102b7fdf9ae5
Face Kapow <facekapow@outlook.com>
2016-08-04 16:47:55 -0400

    Backend-ify graphics subsystem

--- js/core/graphics/index.js
+++ js/core/graphics/index.js
@@ -1,38 +1,26 @@
 'use strict';
 
-const { ioPort } = require('../driver-utils');
-const constants = require('./constants');
-const vbe_dispi_ioport_index = ioPort(constants.VBE_DISPI_IOPORT_INDEX);
-const vbe_dispi_ioport_data = ioPort(constants.VBE_DISPI_IOPORT_DATA);
-
-function writeBgaRegister(index, data) {
-  vbe_dispi_ioport_index.write16(index >>> 0);
-  vbe_dispi_ioport_data.write16(data >>> 0);
-}
-function readBgaRegister(index) {
-  vbe_dispi_ioport_index.write16(index >>> 0);
-  return vbe_dispi_ioport_data.read16();
-}
+const GraphicsRenderer = require('./graphics-renderer');
+const { Screen, symbols: screenSymbols } = require('./screen');
+const renderers = require('./renderers');
+const screen = new Screen();
 
 module.exports = {
   graphicsAvailable() {
-    const bgaVersion = readBgaRegister(constants.VBE_DISPI_INDEX_ID);
-    for (const i of [0, 1, 2, 3, 4, 5]) {
-      if (bgaVersion === constants[`VBE_DISPI_ID${i}`]) {
-        return true;
-      }
-    }
-    return false;
+    return renderers.renderersAvailable();
   },
   enableGraphics(width, height, bitDepth) {
-    writeBgaRegister(constants.VBE_DISPI_INDEX_ENABLE, constants.VBE_DISPI_DISABLED);
-    writeBgaRegister(constants.VBE_DISPI_INDEX_XRES, width);
-    writeBgaRegister(constants.VBE_DISPI_INDEX_YRES, height);
-    writeBgaRegister(constants.VBE_DISPI_INDEX_BPP, bitDepth);
-    writeBgaRegister(constants.VBE_DISPI_INDEX_ENABLE, constants.VBE_DISPI_ENABLED | constants.VBE_DISPI_LFB_ENABLED);
+    const renderer = renderers.getDefaultRenderer();
+    renderer.enableGraphics(width, height, bitDepth);
+    screen[screenSymbols.reset]();
+    screen[screenSymbols.init](width, height, bitDepth, renderer);
   },
-  disableGraphics() {
-    writeBgaRegister(constants.VBE_DISPI_INDEX_ENABLE, constants.VBE_DISPI_DISABLED);
+  GraphicsRenderer,
+  get screen() {
+    return screen;
   },
-  constants,
+  addRenderer: renderers.addRenderer,
+  get constants() {
+    return renderers.getConstants();
+  }
 };
--- js/driver/bga/index.js
+++ js/driver/bga/index.js
@@ -1,25 +1,66 @@
 'use strict';
 
-let buf = null;
-const callbacks = [];
+const { ioPort } = require('../../core/driver-utils');
+const constants = require('./constants');
+const vbe_dispi_ioport_index = ioPort(constants.VBE_DISPI_IOPORT_INDEX);
+const vbe_dispi_ioport_data = ioPort(constants.VBE_DISPI_IOPORT_DATA);
+
+function writeBgaRegister(index, data) {
+  vbe_dispi_ioport_index.write16(index >>> 0);
+  vbe_dispi_ioport_data.write16(data >>> 0);
+}
+function readBgaRegister(index) {
+  vbe_dispi_ioport_index.write16(index >>> 0);
+  return vbe_dispi_ioport_data.read16();
+}
+function bgaAvailable() {
+  const bgaVersion = readBgaRegister(constants.VBE_DISPI_INDEX_ID);
+  for (const i of [0, 1, 2, 3, 4, 5]) {
+    if (bgaVersion === constants[`VBE_DISPI_ID${i}`]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 const driver = {
   init(device) {
-    buf = device.bars[0].resource;
-    for (const cb of callbacks) {
-      cb();
-    }
+    const buf = new Uint8Array(device.bars[0].resource.buffer());
+    const renderer = new runtime.graphics.GraphicsRenderer('bga');
+    renderer.onenablegraphics = (width, height, bitDepth) => {
+      writeBgaRegister(constants.VBE_DISPI_INDEX_ENABLE, constants.VBE_DISPI_DISABLED);
+      writeBgaRegister(constants.VBE_DISPI_INDEX_XRES, width);
+      writeBgaRegister(constants.VBE_DISPI_INDEX_YRES, height);
+      writeBgaRegister(constants.VBE_DISPI_INDEX_BPP, bitDepth);
+      writeBgaRegister(constants.VBE_DISPI_INDEX_ENABLE, constants.VBE_DISPI_ENABLED | constants.VBE_DISPI_LFB_ENABLED);
+    };
+    renderer.ongetpixel = (x, y) => {
+      const where = ((y * runtime.graphics.screen.width) + x) * (runtime.graphics.screen.bitDepth / 8);
+      return {
+        get r() {
+          return buf[where + 2];
+        },
+        get g() {
+          return buf[where + 1];
+        },
+        get b() {
+          return buf[where];
+        },
+        set r(val) {
+          buf[where + 2] = val;
+        },
+        set g(val) {
+          buf[where + 1] = val;
+        },
+        set b(val) {
+          buf[where] = val;
+        },
+      };
+    };
+    renderer.constants = constants;
+    runtime.graphics.addRenderer(renderer);
   },
   reset() {},
 };
 
-runtime.graphics.getDisplayBuffer = () => new Promise((resolve, reject) => {
-  if (buf) {
-    resolve(buf.buffer());
-  } else {
-    callbacks.push(() => {
-      resolve(buf.buffer());
-    });
-  }
-});
 runtime.pci.addDriver(0x1234, 0x1111, driver);
